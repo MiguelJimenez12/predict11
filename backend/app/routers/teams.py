6c2285eccbb89bfe46e7315c7d5342314bc4870d
Miguel Jiménez <miguelj@ciencias.unam.mx>
2026-07-16 15:06:12 -0600

    :zap: feat: feat(api): teams endpoints

--- backend/app/routers/teams.py
+++ backend/app/routers/teams.py
@@ -1,7 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.schemas.team import Team
-
-from app.services.team_service import get_all_teams
+from app.services.team_service import (get_all_teams, get_team_by_id)
 
 router = APIRouter()
 
@@ -9,3 +8,16 @@ router = APIRouter()
 @router.get("/teams", response_model=list[Team])
 def get_teams():
     return get_all_teams()
+
+@router.get("/teams/{team_id}", response_model=Team)
+def get_team(team_id: int):
+
+    team = get_team_by_id(team_id)
+
+    if team is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Team not found"
+        )
+
+    return team
--- backend/app/services/team_service.py
+++ backend/app/services/team_service.py
@@ -11,3 +11,12 @@ def get_all_teams():
     ]
 
     return teams
+
+def get_team_by_id(team_id: int):
+    teams = get_all_teams()
+
+    for team in teams:
+        if team["id"] == team_id:
+            return team
+
+    return None
