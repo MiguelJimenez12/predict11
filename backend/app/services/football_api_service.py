fc70a4334c938d3856cc81a7182e95745614ecad
Miguel Jiménez <miguelj@ciencias.unam.mx>
2026-07-20 15:01:17 -0600

    :zap: feat(api): conexion con API-Sports

---
+++ backend/app/services/football_api_service.py
@@ -0,0 +1,36 @@
+import os
+
+import httpx
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("FOOTBALL_API_KEY")
+
+BASE_URL = "https://v3.football.api-sports.io"
+
+
+def get_teams():
+
+    url = f"{BASE_URL}/teams"
+
+    headers = {
+        "x-apisports-key": API_KEY
+    }
+
+    params = {
+        "league": 262,
+        "season": 2024
+    }
+
+    response = httpx.get(
+        url,
+        headers=headers,
+        params=params
+    )
+
+    response.raise_for_status()
+
+    data = response.json()
+
+    return data["response"]
---
+++ backend/test_api.py
@@ -0,0 +1,5 @@
+from app.services.football_api_service import get_teams
+
+teams = get_teams()
+
+print(teams)
